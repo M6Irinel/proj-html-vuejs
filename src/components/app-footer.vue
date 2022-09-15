ac9403b84d58c67753a7365b4527df8d21568aeb
M6Irinel <momoloacairinel666@gmail.com>
2022-09-15 21:32:08 +0200

    add 1° footer

--- src/components/app-footer.vue
+++ src/components/app-footer.vue
@@ -2,7 +2,42 @@
     <footer>
         <section>
             <div class="myContainer">
-                footer
+                <div class="d-flex">
+                    <address>
+                        <h4>Adress</h4>
+                        <div>
+                            <p>382 NE 191st St # 87394 Miami, FL 33179-3899</p>
+                            <p>+1 (305) 547-9909 (9am - 5pm EST, Monday - Friday)</p>
+                            <p><a href="#">support@maxcoach.com</a></p>
+                            <div class="icons">
+                                <button v-for="(el, i) in arrayIconFooter" :key="i">
+                                    <font-awesome-icon :icon="['fa-brands', el]" />
+                                </button>
+                            </div>
+                        </div>
+                    </address>
+                    <div class="explore">
+                        <h4>Explore</h4>
+                        <div>
+                            <p><a href="#">Start here</a></p>
+                            <p><a href="#">Success story</a></p>
+                            <p><a href="#">Blog</a></p>
+                            <p><a href="#">Courses</a></p>
+                            <p><a href="#">About us</a></p>
+                            <p><a href="#">Contact us</a></p>
+                        </div>
+                    </div>
+                    <div class="info">
+                        <h4>Information</h4>
+                        <div>
+                            <p><a href="#">Membership</a></p>
+                            <p><a href="#">Purchase guide</a></p>
+                            <p><a href="#">Privacy policy</a></p>
+                            <p><a href="#">Terms of services</a></p>
+                        </div>
+                    </div>
+                </div>
+                <p class="endPageWeb text-center mt-5 mb-0">© 2020 Maxcoach. All Rights Reserved</p>
             </div>
         </section>
     </footer>
@@ -10,14 +45,97 @@
 
 
 <script>
+import arrayIconFooter from './json-components/array-icon-footer.json';
+
 export default {
     name: 'appFooter',
 
-    
+    data () {
+        return {
+            arrayIconFooter
+        }
+    },
 }
 </script>
 
 
 <style scoped lang="scss">
-    
+@import url('https://fonts.googleapis.com/css2?family=Rubik:wght@500;600;700&display=swap');
+
+section {
+    background-color: #f8f8f8;
+    padding: 69px 0 50px;
+}
+
+h4 {
+    margin-bottom: 20px;
+}
+
+p {
+    font-size: 0.85rem;
+    font-weight: 500;
+}
+
+p,
+a {
+    color: #6f6f6f;
+}
+
+a {
+    text-decoration: none;
+    transition: color 0.2s ease-in-out;
+
+    &:hover {
+        color: #20ad96;
+    }
+}
+
+address {
+    flex-basis: 0;
+    flex-grow: 6;
+
+    p {
+        font-family: 'Rubik', sans-serif;
+    }
+
+    .icons {
+        display: flex;
+        gap: 20px;
+        font-size: 1.5rem;
+
+        & > button {
+            opacity: 0.3;
+            transition: all 0.2s ease-in-out;
+
+            &:hover {
+                opacity: 1;
+                color: #20ad96;
+            }
+        }
+    }
+}
+
+.explore {
+    flex-basis: 0;
+    flex-grow: 3;
+
+    & > div {
+        display: flex;
+        flex-wrap: wrap;
+
+        & > * {
+            flex-basis: 40%;
+            flex-grow: 1;
+        }
+    }
+}
+
+.info {
+    flex-basis: 0;
+    flex-grow: 3;
+}
+
+.endPageWeb {
+    color: #9c9c9c;
+}
 </style>
--- src/components/imports-library/fontawesome.js
+++ src/components/imports-library/fontawesome.js
@@ -3,6 +3,7 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { faCircleUser, faUser, faFileLines, faCalendar, faEye } from '@fortawesome/free-regular-svg-icons';
 import { faMagnifyingGlass, faChevronDown, faDownload, faArrowRightLong, faRightLong } from '@fortawesome/free-solid-svg-icons';
+import { faSquareFacebook, faTwitter, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 library.add(
     faCircleUser,
     faMagnifyingGlass,
@@ -13,6 +14,10 @@ library.add(
     faFileLines,
     faRightLong,
     faCalendar,
-    faEye
+    faEye,
+    faSquareFacebook,
+    faTwitter,
+    faInstagram,
+    faLinkedin
 );
 Vue.component( 'font-awesome-icon', FontAwesomeIcon );
